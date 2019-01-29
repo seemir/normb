68a6a9564674f9d45658610be10b5c1b3bfdb9c3
seemir <samir.adrik@gmail.com>
2019-01-30 00:21:17 +0100

    Finished first version of pdf functionality (needs refactoring)

--- normbatt/normality_battery.py
+++ normbatt/normality_battery.py
@@ -65,7 +65,54 @@ class NormalityBattery:
         """
         return "x".join(str(dim) for dim in self.df.shape)
 
-    def check_univariate_normality(self, dim='col', digits=5, ds=False):
+    def print_descriptive_statistics(self, dim='col', digits=5):
+        """
+        Get string of descriptive statistics
+
+        Parameters
+        ----------
+        dim     : string
+                  indicate whether one wants to test for normality along the columns 'col' or rows
+                  'row', default is 'col'
+        digits  : integer
+                  number of decimal places to round down
+
+        Returns
+        -------
+        Out     : str
+                  string containing descriptive statistics
+
+        """
+        DataFrameGenerator.evaluate_data_type({dim: str, digits: int})
+
+        desc_table = PrettyTable(vrules=2)
+        rnd, d = round, digits
+        dim_name = 'col' if dim == 'col' else 'row'
+
+        decs_header_names = [dim_name,
+                             'mean', 'median',
+                             'variance', 'stdev',
+                             'kurtosis', 'skewness',
+                             'min', 'max',
+                             'quant (95%)']
+        desc_table.field_names = decs_header_names
+
+        vectors = self.df.iteritems() if dim == "col" else self.df.iterrows()
+
+        for i, vector in vectors:
+            desc_row = [rnd(i + 1, d),
+                        rnd(np.mean(vector), d), rnd(np.median(vector), d),
+                        rnd(np.var(vector), d), rnd(np.std(vector), d),
+                        rnd(stats.kurtosis(vector), d), rnd(stats.skew(vector), d),
+                        rnd(min(vector), d), rnd(max(vector), d),
+                        rnd(np.quantile(vector, 0.95), d)]
+            desc_table.add_row(desc_row)
+            desc_table.align = "r"
+
+        desc_table.title = 'Descriptive statistics ' + self.get_dimensions() + ' DataFrame(df)'
+        return str(desc_table)
+
+    def print_univariate_normality(self, dim='col', digits=5):
         """
         Checks to see if the values in the rows or columns of a dataframe are univariate normally
         distributed using Jarque-Bera, D’Agostino / Pearson’s, Kolmogorov–Smirnov and Shapiro-Wilk.
@@ -77,30 +124,18 @@ class NormalityBattery:
                   'row', default is 'col'
         digits  : integer
                   number of decimal places to round down
-        ds      : bool
-                  indicating if one wants additional table with descriptive statistics
-                  of the data
 
         Returns
         -------
-        Out     : prettytable
-                  table containing test-statistic and p-value of row/col vectors
+        Out     : str
+                  string containing test-statistic and p-value of row/col vectors
 
         """
-        DataFrameGenerator.evaluate_data_type({dim: str, digits: int, ds: bool})
+        DataFrameGenerator.evaluate_data_type({dim: str, digits: int})
 
-        norm_table, desc_table = PrettyTable(vrules=2), PrettyTable(vrules=2)
+        norm_table = PrettyTable(vrules=2)
         rnd, d = round, digits
         dim_name = 'col' if dim == 'col' else 'row'
-
-        if ds:
-            decs_header_names = [dim_name,
-                                 'mean', 'median',
-                                 'variance', 'stdev',
-                                 'kurtosis', 'skewness',
-                                 'min', 'max',
-                                 'quant (95%)']
-            desc_table.field_names = decs_header_names
 
         norm_header_names = [dim_name,
                              'jb', 'p-value (jb)',
@@ -111,16 +146,6 @@ class NormalityBattery:
 
         vectors = self.df.iteritems() if dim == "col" else self.df.iterrows()
         for i, vector in vectors:
-            if ds:
-                desc_row = [rnd(i + 1, d),
-                            rnd(np.mean(vector), d), rnd(np.median(vector), d),
-                            rnd(np.var(vector), d), rnd(np.std(vector), d),
-                            rnd(stats.kurtosis(vector), d), rnd(stats.skew(vector), d),
-                            rnd(min(vector), d), rnd(max(vector), d),
-                            rnd(np.quantile(vector, 0.95), d)]
-                desc_table.add_row(desc_row)
-                desc_table.align = "r"
-
             jb, p_jb = stats.jarque_bera(vector)
             k2, p_pr, = stats.normaltest(vector)
             ks, p_ks = stats.kstest(vector, cdf='norm')
@@ -134,19 +159,17 @@ class NormalityBattery:
             norm_table.add_row(norm_row)
             norm_table.align = "r"
 
-        desc_table.title = 'Descriptive statistics ' + self.get_dimensions() + ' DataFrame(df)'
         norm_table.title = 'Normality test ' + self.get_dimensions() + ' DataFrame(df)'
+        return str(norm_table)
 
-        return list(map(str, [desc_table, norm_table])) if ds else str(norm_table)
-
-    def print_report(self, filename="NormalityReport.txt", file_dir="reports/", dim='col', digits=5,
-                     ds=False, pdf=False):
+    def print_report(self, file_name="NormalityReport.txt", file_dir="reports/", dim='col',
+                     digits=5, ds=False, pdf=False):
         """
         Method that prints a report containing the results of the Normality tests
 
         Parameters
         ----------
-        filename    : str
+        file_name    : str
                       name of file to be produced
         file_dir    : str
                       directory to save the file
@@ -158,9 +181,12 @@ class NormalityBattery:
         ds          : bool
                       indicating if one wants additional table with descriptive
                       statistics of the data
+        pdf         : bool
+                      indicating whether one wants to print report as pdf
+
         """
         DataFrameGenerator.evaluate_data_type(
-            {filename: str, file_dir: str, dim: str, digits: int, ds: bool})
+            {file_name: str, file_dir: str, dim: str, digits: int, ds: bool})
 
         try:
             if not os.path.exists(file_dir):
@@ -168,19 +194,25 @@ class NormalityBattery:
         except Exception as e:
             raise OSError("creation of dir " + file_dir + " failed with: " + str(e))
 
-        file = open(os.path.join(file_dir, filename), "w")
+        file = open(os.path.join(file_dir, file_name), "w")
+        # TODO: Everything below needs refactoring asap, i.e. to much is hardcoded!
         if ds:
-            file.write(self.check_univariate_normality(dim, digits, ds=ds)[0] + '\n')
-            file.write(self.check_univariate_normality(dim, digits, ds=ds)[1])
-            if pdf:  # TODO: Fix this shit!
-                pw = PDFWriter()
+            file.write(self.print_descriptive_statistics(dim, digits) + '\n')
+            file.write(self.print_univariate_normality(dim, digits))
+            if pdf:
+                pw = PDFWriter(file_name[:-4] + '.pdf', file_dir)
                 pw.set_font('Courier', 8.5)
-                pw.set_header('Normality Report')
-                pw.set_footer('Version 0.0.1')
-                for lines in self.check_univariate_normality(dim, digits, ds=ds)[0]:
-                    for line in lines.split('n'):
-                        pw.write_line(line)
+                for line in self.print_descriptive_statistics(dim, digits).split('\n'):
+                    pw.write_line(line)
+                for line in self.print_univariate_normality(dim, digits).split('\n'):
+                    pw.write_line(line)
                 pw.close()
         else:
-            file.write(self.check_univariate_normality(dim, digits, ds=ds))
+            file.write(self.print_univariate_normality(dim, digits))
+            if pdf:
+                pw = PDFWriter(file_name[:-4] + '.pdf', file_dir)
+                pw.set_font('Courier', 8.5)
+                for line in self.print_univariate_normality(dim, digits).split('\n'):
+                    pw.write_line(line)
+                pw.close()
         file.close()
